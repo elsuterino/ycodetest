54bccfca8fc5e7996296b2bc699c6e8c5e6a2bdd
Vyatuatas Riauka <vytautas.riauka@mtcmedia.co.uk>
2020-03-13 22:12:57 +0200

    readme

--- app/Website.php
+++ app/Website.php
@@ -4,6 +4,24 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Website
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $url
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Website newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Website newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Website query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Website whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Website whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Website whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Website whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Website whereUrl($value)
+ * @mixin \Eloquent
+ */
 class Website extends Model
 {
     protected $guarded = [];
