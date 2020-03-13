a2f778dc66e71fc58b9868eea4d386e7c3d4ebc1
Vyatuatas Riauka <vytautas.riauka@mtcmedia.co.uk>
2020-03-13 21:55:02 +0200

    seeder

--- database/seeds/WebsiteTableSeeder.php
+++ database/seeds/WebsiteTableSeeder.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class WebsiteTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-         $this->call(WebsiteTableSeeder::class);
+        factory(\App\Website::class, 50)->create();
     }
 }
